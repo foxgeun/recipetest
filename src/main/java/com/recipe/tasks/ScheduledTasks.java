125d9f46220465b54e17be2374e51313c413bee8
kmingyuyu <skyjangking@naver.com>
2023-08-17 16:46:42 +0900

    update

--- src/main/java/com/recipe/tasks/ScheduledTasks.java
+++ src/main/java/com/recipe/tasks/ScheduledTasks.java
@@ -22,18 +22,18 @@ public class ScheduledTasks {
     
     
     
-   
-    @Scheduled(fixedRate = 3600000) // 1시간 마다 업데이트
-    public void crawlAndSaveScheduled() {
-    	
-        try {
-
-            crawlerListService.crawlAndSaveRecipes();
-            crawlerService.crawlAndSaveRecipes();
-        } catch (IOException e) {
-            // 예외 처리 및 로그 출력
-            e.printStackTrace();
-        }
-    }
+//   
+//    @Scheduled(fixedRate = 3600000) // 1시간 마다 업데이트
+//    public void crawlAndSaveScheduled() {
+//    	
+//        try {
+//
+//            crawlerListService.crawlAndSaveRecipes();
+//            crawlerService.crawlAndSaveRecipes();
+//        } catch (IOException e) {
+//            // 예외 처리 및 로그 출력
+//            e.printStackTrace();
+//        }
+//    }
 }
 
